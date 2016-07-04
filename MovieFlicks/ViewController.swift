05cb9d3cad7b5583a36a6a945988b589c003669f
Nguyen Truong Ky <nguyentruongky3390@yahoo.com>
2016-07-05 04:38:40 +0700

    Init project

---
+++ MovieFlicks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieFlicks
+//
+//  Created by Ky Nguyen on 7/4/16.
+//  Copyright © 2016 Ky Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
