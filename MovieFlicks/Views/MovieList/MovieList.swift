348d93afa47c4262e2244adc3dca371c68c9ff4e
Nguyen Truong Ky <nguyentruongky3390@yahoo.com>
2016-07-05 21:42:00 +0700

    Add list view mode

---
+++ MovieFlicks/Views/MovieList/MovieList.swift
@@ -0,0 +1,21 @@
+//
+//  MovieList.swift
+//  MovieFlicks
+//
+//  Created by Ky Nguyen on 7/5/16.
+//  Copyright © 2016 Ky Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class MovieList: KViewBase {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ MovieFlicks/Views/MovieList/MovieTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableCell.swift
+//  MovieFlicks
+//
+//  Created by Ky Nguyen on 7/5/16.
+//  Copyright © 2016 Ky Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
